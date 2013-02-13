be108589747e405bc9e62f36c539e54f44f6978f
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-02-13 19:22:37 +0000

    Tela de Login, desenho

--- ganesh.swing/src/ganesh/swing/Starter.java
+++ ganesh.swing/src/ganesh/swing/Starter.java
@@ -1,12 +1,20 @@
 package ganesh.swing;
 
+import ganesh.swing.ui.login.Login;
+
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
 import org.scharlessantos.hermes.Hermes;
 
 /**
  * 
  */
 public class Starter {
-
+	
+	
 	/**
 	 * Inicia o GANESH Swing Client
 	 */
@@ -17,5 +25,33 @@ public class Starter {
 				"\n An open source program: NO WARRANTY" +
 				"\n=====================================");
 
+		try {
+			Hermes.info("NimbusLookAndFeel");
+			UIManager.setLookAndFeel(new NimbusLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			Hermes.warn("No nimbus look and feel");
+		}
+		
+		GaneshStart start = new GaneshStart();
+		start.setName("Ganesh Start Thread");
+		start.start();
+		
+	}
+	
+	private static class GaneshStart extends Thread{
+
+		private Login login;
+
+		
+		@Override
+		public void run() {
+			if (login==null)
+				login = new Login();
+			
+			login.setLocationRelativeTo(null);
+			
+			login.setVisible(true);
+		}
+		
 	}
 }
---
+++ ganesh.swing/src/ganesh/swing/ui/login/Login.java
@@ -0,0 +1,108 @@
+package ganesh.swing.ui.login;
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.scharlessantos.hermes.Hermes;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.BevelBorder;
+
+
+public class Login extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5127775206753150588L;
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JTextField txtSenha;
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setResizable(false);
+		setTitle("Ganesh Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 473, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel();
+		label.setBounds(12, 0, 166, 255);
+		label.setIcon(new ImageIcon("/home/charles/workspaces/farinha/ganesh.swing/images/ganesh.png"));
+		contentPane.add(label);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBounds(189, 12, 260, 202);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(13, 12, 70, 15);
+		panel.add(lblUsuario);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(12, 36, 236, 38);
+		panel.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setBounds(13, 78, 70, 15);
+		panel.add(lblSenha);
+		
+		txtSenha = new JTextField();
+		txtSenha.setBounds(12, 105, 236, 38);
+		panel.add(txtSenha);
+		txtSenha.setColumns(10);
+		
+		JButton btnAcessar = new JButton("Acessar");
+		btnAcessar.setBounds(12, 147, 117, 38);
+		panel.add(btnAcessar);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.setBounds(131, 147, 117, 38);
+		panel.add(btnSair);
+		
+		JButton btnConfigurar = new JButton("C");
+		btnConfigurar.setBounds(417, 230, 32, 25);
+		contentPane.add(btnConfigurar);
+		btnSair.addActionListener(new btnSairListener());
+		btnAcessar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+	}
+	
+	@Override
+	public void setVisible(boolean b) {
+		Hermes.info("starting...");
+		super.setVisible(b);
+	}
+
+	
+	private class btnSairListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (JOptionPane.showConfirmDialog(Login.this,"Deseja realmente sair?", "Saindo...",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)==JOptionPane.YES_OPTION)
+				System.exit(0);
+			else
+				txtUsuario.requestFocus();
+		}
+		
+	}
+
+}
