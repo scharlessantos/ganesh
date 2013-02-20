be1ea69fded31544f6179516c2ea6cc0383db59f
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-02-20 23:38:00 +0000

    initdb #1

--- ganesh/src/ganesh/embed/database/DBServer.java
+++ ganesh/src/ganesh/embed/database/DBServer.java
@@ -1,7 +1,15 @@
 package ganesh.embed.database;
 
+import ganesh.exceptions.GException;
+
+import org.scharlessantos.hermes.Hermes;
+
 public class DBServer {
 	
-	
+	public static void initDB() throws GException{
+		Hermes.info("Iniciando Banco de Dados");
+		
+		Hermes.info("Banco de Dados atualizado");
+	}
 
 }
