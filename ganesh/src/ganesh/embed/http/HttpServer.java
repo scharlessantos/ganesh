ad7d916d2362f013b1ba9f3f0ee0c62bb60ab8dc
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-02-23 16:50:10 +0000

    Novas libs, http server

--- ganesh/src/ganesh/embed/http/HttpServer.java
+++ ganesh/src/ganesh/embed/http/HttpServer.java
@@ -1,5 +1,34 @@
 package ganesh.embed.http;
 
+import ganesh.exceptions.ErrorCode;
+import ganesh.exceptions.GException;
+
+import org.eclipse.jetty.server.Server;
+
 public class HttpServer {
 
+	
+	private static HttpServer instance;
+	
+	public static HttpServer getInstance(){
+		if (instance==null)
+			instance = new HttpServer();
+		
+		return instance;
+	}
+	
+	private HttpServer(){};
+	
+	private Server server;
+	
+	public void startServer() throws GException{
+		server = new Server(8833);
+		try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			throw new GException(ErrorCode.UNKOWN, "Não foi possivel iniciar o servidor http", e);
+		}
+	}
+	
 }
