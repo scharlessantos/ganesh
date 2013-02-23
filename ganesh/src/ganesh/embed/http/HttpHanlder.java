cf9d7599c40c2ec605319065cc6e29e5ce4b2436
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-02-23 17:08:00 +0000

    javax.servlet

---
+++ ganesh/src/ganesh/embed/http/HttpHanlder.java
@@ -0,0 +1,24 @@
+package ganesh.embed.http;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HttpHanlder extends AbstractHandler {
+
+	@Override
+	public void handle(String arg0, Request baseReq, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		resp.setContentType("text/html;charset=utf-8");
+		resp.setStatus(Response.SC_OK);
+		baseReq.setHandled(true);
+		resp.getWriter().println("Helo World");
+	
+		
+	}
+}
