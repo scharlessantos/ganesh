d9dbf82940fc743c8d64a8d4e62a0e158746018e
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-03-05 22:07:39 +0000

    firulas

--- ganesh/src/ganesh/embed/http/servlets/E404Servlet.java
+++ ganesh/src/ganesh/embed/http/servlets/E404Servlet.java
@@ -1,7 +1,6 @@
 /* Copyright (c) 2013 G.I.C Consultoria e Comunicação Ltda */
 package ganesh.embed.http.servlets;
 
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -24,8 +23,9 @@ public class E404Servlet extends HttpServlet {
 		resp.setContentType("text/html;charset=utf-8");
 		resp.setStatus(Response.SC_OK);
 		resp.getWriter().println("<center>");
+		resp.getWriter().println("<font size=6 color=red><i><b>Erro Interno<br></b></i></font>");
 		resp.getWriter().println("<img src=/img/e404 />");
-		resp.getWriter().println("<font size=2 color=red><i><br>Não foi possível encontrar <b>" + req.getAttribute("target") + "</b></i></font>");
+		resp.getWriter().println("<font size=2 color=red><i><br>Erro 404 - Não foi possível encontrar <b>" + req.getAttribute("target") + "</b></i></font>");
 		resp.getWriter().println("</center>");
 
 		resp.getWriter().println(InfoServlet.poweredBy());
