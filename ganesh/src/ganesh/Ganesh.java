25dc852bb4caa1759443f0d46d676ce33e198560
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-03-30 02:27:17 +0000

    Cria usuário root se não tiver

--- ganesh/src/ganesh/Ganesh.java
+++ ganesh/src/ganesh/Ganesh.java
@@ -1,7 +1,9 @@
 /* Ganesh Server, developed in 2013*/
 package ganesh;
 
+import ganesh.common.exceptions.GException;
 import ganesh.common.i18n.GaneshI18n;
+import ganesh.db.Usuario;
 import ganesh.i18n.GMessages;
 import ganesh.i18n.Messages;
 import ganesh.programs.ProgramManager;
@@ -30,14 +32,31 @@ public class Ganesh {
 		GaneshI18n.loadTranslations(Language.EN_US, this.getClass().getResourceAsStream("i18n/translations/messages.en_US.trd"));
 
 		registerPrograms();
+
+		try {
+			Usuario usuario = Usuario.getByUsername("root");
+
+			if (usuario != null)
+				Hermes.info(usuario.toString());
+			else {
+				usuario = new Usuario();
+				usuario.setCodigo("root");
+				usuario.setNome("Super Usuário");
+				usuario.setContato("root@ganesh");
+				usuario.setDocumento("GANESH");
+				usuario.setEndereco("R. India");
+				usuario.setSenha("123456");
+				usuario.setUsername("root");
+				usuario.save();
+			}
+
+		} catch (GException e) {
+			Hermes.error(e);
+		}
 	}
 
 	private void registerPrograms() {
-
-		Hermes.info(Ganesh.getGMessages().arquivoDeDadosExistePoremNaoEhUmDiretorio().toString());
-
 		ProgramManager.registerProgram("grupo", PrgGrupo.class);
-
 	}
 
 	public static Messages getMessages() {
