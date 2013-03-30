925bde416d8e7b100722b3251e6dfc544eb36ef3
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-03-30 00:52:14 +0000

    i18n class generator

--- ganesh.common/src/ganesh/common/exceptions/GException.java
+++ ganesh.common/src/ganesh/common/exceptions/GException.java
@@ -1,22 +1,37 @@
 /* Ganesh Commons, developed in 2013*/
 package ganesh.common.exceptions;
 
+import ganesh.common.i18n.GString;
+
 public class GException extends Exception {
 
 	private static final long serialVersionUID = 7414687980700018664L;
 
 	private ErrorCode code;
 
+	private GString message;
+
 	public GException(ErrorCode code) {
-		this(code, "");
+		this(code, "", null);
 	}
 
-	public GException(ErrorCode code, String message) {
+	public GException(ErrorCode code, GString message) {
 		this(code, message, null);
 	}
 
 	public GException(ErrorCode code, Throwable cause) {
-		this(code, null, cause);
+		this(code, "", cause);
+	}
+
+	public GException(ErrorCode code, GString message, Throwable cause) {
+		super(cause);
+
+		this.code = code;
+		this.message = message;
+	}
+
+	public GException(ErrorCode code, String message) {
+		this(code, message, null);
 	}
 
 	public GException(ErrorCode code, String message, Throwable cause) {
@@ -31,6 +46,6 @@ public class GException extends Exception {
 
 	@Override
 	public String getMessage() {
-		return code.toString() + ": " + super.getMessage();
-	};
+		return (code == null ? "" : code.toString() + ": ") + (message == null ? super.getMessage() : message.toString());
+	}
 }
--- ganesh.common/src/ganesh/common/i18n/GString.java
+++ ganesh.common/src/ganesh/common/i18n/GString.java
@@ -1,6 +1,47 @@
 /* Ganesh Commons, developed in 2013 */
 package ganesh.common.i18n;
 
+import java.util.Arrays;
+
 public final class GString {
+
+	private final String format;
+	private final Object[] args;
+
+	public GString(String str) {
+		format = str;
+		args = null;
+	}
+
+	public GString(String format, Object... args) {
+		String.format(format, args);
+		this.format = format;
+		this.args = args;
+	}
+
+	@Override
+	public int hashCode() {
+		if (args == null)
+			return format.hashCode();
+		else
+			return 31 * format.hashCode() + Arrays.hashCode(args);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof GString) {
+			GString o = (GString)obj;
+			if (args == null)
+				return o.args == null && format.equals(o.format);
+			else
+				return format.equals(o.format) && Arrays.equals(args, o.args);
+		} else
+			return false;
+	}
+
+	@Override
+	public String toString() {
+		return GaneshI18n.translate(format, args);
+	}
 
 }
