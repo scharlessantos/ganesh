15e527ac5d1c39e548fee98c7595bd24c6089ea6
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-04-10 23:27:07 +0000

    GPL notice

--- ganesh.swing/src/ganesh/swing/Starter.java
+++ ganesh.swing/src/ganesh/swing/Starter.java
@@ -20,11 +20,18 @@ public class Starter {
 	 * Inicia o GANESH Swing Client
 	 */
 	public static void main(String[] args) {
-		Hermes.info("" +
-			"\n=====================================" +
-			"\n    Starting Ganesh Swing Client" +
-			"\n An open source program: NO WARRANTY" +
-			"\n=====================================");
+		Hermes.warn("\nGANESH NOTICE:\nCopyright (C) 2013 Rafael Antonio Farinha (rafarinha123@gmail.com)\n\n" +
+			"This program is free software: you can redistribute it and/or modify\n" +
+			"it under the terms of the GNU General Public License as published by\n" +
+			"the Free Software Foundation, either version 3 of the License, or\n" +
+			"(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\n" +
+			"but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+			"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n" +
+			"GNU General Public License for more details.\n" +
+			"You should have received a copy of the GNU General Public License\n" +
+			"along with this program. If not, see <http://www.gnu.org/licenses/>.\n");
+
+		Hermes.info("============================ Starting Ganesh Swing");
 
 		try {
 			Hermes.info("NimbusLookAndFeel");
