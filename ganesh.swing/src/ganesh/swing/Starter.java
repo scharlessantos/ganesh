c8bad213234c83950360cd2d58fba0c33612560f
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-05-31 21:33:53 +0000

    Quando rodando pelo eclipse, ordena os trds e acrescenta uma linha comentada com as traduções faltantes

--- ganesh.swing/src/ganesh/swing/Starter.java
+++ ganesh.swing/src/ganesh/swing/Starter.java
@@ -2,13 +2,23 @@
 package ganesh.swing;
 
 import ganesh.common.i18n.GaneshI18n;
+import ganesh.swing.i18n.GMessages;
+import ganesh.swing.i18n.Messages;
 import ganesh.swing.ui.login.Login;
 import ganesh.swing.ui.main.GaneshMain;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -75,6 +85,78 @@ public class Starter {
 
 		GaneshI18n.loadTranslations(Language.PT_BR, Starter.class.getResourceAsStream("i18n/translations/messages.pt_BR.trd"));
 		GaneshI18n.loadTranslations(Language.EN_US, Starter.class.getResourceAsStream("i18n/translations/messages.en_US.trd"));
+
+		if (new File(".svn").isDirectory()) {
+			//Se estiver rodando no eclipse, gera o arquivo trd com as traduções que faltam
+
+			Language[] lang = { Language.PT_BR, Language.EN_US };
+
+			for (Language l: lang) {
+				Map<String, String> tradutor = new HashMap<>();
+				GaneshI18n.setLanguage(l);
+
+				List<Method> methods = new ArrayList<>();
+
+				methods.addAll(Arrays.asList(Messages.class.getMethods()));
+				methods.addAll(Arrays.asList(GMessages.class.getMethods()));
+
+				for (Method m: methods) {
+					String key = m.getName();
+
+					key += "(";
+
+					String param = null;
+
+					for (Class<?> p: m.getParameterTypes())
+						param = param == null ? p.getSimpleName() : param + ", " + p.getSimpleName();
+
+					if (param != null)
+						key += param;
+
+					key += ")";
+
+					if (!tradutor.containsKey(key)) {
+						String traducao = GaneshI18n.translate(key);
+
+						if (traducao.equals(key))
+							traducao = null;
+
+						tradutor.put(key, traducao);
+					}
+
+				}
+
+				List<String> keys = new ArrayList<>(tradutor.keySet());
+
+				Collections.sort(keys);
+
+				try {
+					FileWriter fw = new FileWriter(new File("src/ganesh/swing/i18n/translations/messages." + l.getAcronym() + ".trd"));
+
+					for (String key: keys) {
+						String traducao = tradutor.get(key);
+
+						if (traducao == null)
+							fw.append("#");
+
+						fw.append(key);
+						fw.append("=");
+
+						if (traducao != null)
+							fw.append(traducao);
+
+						fw.append("\n");
+					}
+
+					fw.close();
+
+				} catch (IOException e) {
+					Hermes.error(e);
+				}
+
+			}
+
+		}
 
 		GaneshSwing.setLanguage(Language.PT_BR);
 
