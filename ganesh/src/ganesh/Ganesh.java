b404fbde98181c33e5d5264019a8cbb3af040bfe
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-05-31 22:06:47 +0000

    Quando rodando pelo eclipse, ordena os trds e acrescenta uma linha comentada com as traduções faltantes

--- ganesh/src/ganesh/Ganesh.java
+++ ganesh/src/ganesh/Ganesh.java
@@ -10,10 +10,18 @@ import ganesh.programs.ProgramManager;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.scharlessantos.atlas.Language;
 import org.scharlessantos.hermes.Hermes;
@@ -45,6 +53,78 @@ public class Ganesh {
 
 		GaneshI18n.loadTranslations(Language.PT_BR, Starter.class.getResourceAsStream("i18n/translations/messages.pt_BR.trd"));
 		GaneshI18n.loadTranslations(Language.EN_US, Starter.class.getResourceAsStream("i18n/translations/messages.en_US.trd"));
+
+		if (new File(".svn").isDirectory()) {
+			//Se estiver rodando no eclipse, gera o arquivo trd com as traduções que faltam
+
+			Language[] lang = { Language.PT_BR, Language.EN_US };
+
+			for (Language l: lang) {
+				Map<String, String> tradutor = new HashMap<>();
+				GaneshI18n.setLanguage(l);
+
+				List<Method> methods = new ArrayList<>();
+
+				methods.addAll(Arrays.asList(Messages.class.getMethods()));
+				methods.addAll(Arrays.asList(GMessages.class.getMethods()));
+
+				for (Method m: methods) {
+					String key = m.getName();
+
+					key += "(";
+
+					String param = null;
+
+					for (Class<?> p: m.getParameterTypes())
+						param = param == null ? p.getSimpleName() : param + ", " + p.getSimpleName();
+
+					if (param != null)
+						key += param;
+
+					key += ")";
+
+					if (!tradutor.containsKey(key)) {
+						String traducao = GaneshI18n.translate(key);
+
+						if (traducao.equals(key))
+							traducao = null;
+
+						tradutor.put(key, traducao);
+					}
+
+				}
+
+				List<String> keys = new ArrayList<>(tradutor.keySet());
+
+				Collections.sort(keys);
+
+				try {
+					FileWriter fw = new FileWriter(new File("src/ganesh/i18n/translations/messages." + l.getAcronym() + ".trd"));
+
+					for (String key: keys) {
+						String traducao = tradutor.get(key);
+
+						if (traducao == null)
+							fw.append("#");
+
+						fw.append(key);
+						fw.append("=");
+
+						if (traducao != null)
+							fw.append(traducao);
+
+						fw.append("\n");
+					}
+
+					fw.close();
+
+				} catch (IOException e) {
+					Hermes.error(e);
+				}
+
+			}
+
+		}
 
 		ProgramManager.loadPrograms();
 	}
