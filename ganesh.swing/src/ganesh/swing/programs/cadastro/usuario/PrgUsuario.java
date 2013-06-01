a79107796bbc4aca5e00f7679e9b3efdcb28042f
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-06-01 11:53:16 +0000

    Programa de Usuario sai do menu

--- ganesh.swing/src/ganesh/swing/programs/cadastro/usuario/PrgUsuario.java
+++ ganesh.swing/src/ganesh/swing/programs/cadastro/usuario/PrgUsuario.java
@@ -1,10 +1,8 @@
 /* Ganesh Swing Client, developed in 2013 */
 package ganesh.swing.programs.cadastro.usuario;
 
-import ganesh.swing.ProgramManager.Menu;
 import ganesh.swing.programs.GaneshProgram;
 import ganesh.swing.programs.ProgramDescriptor;
-import ganesh.swing.ui.images.Images.Icons;
 
 public class PrgUsuario extends GaneshProgram {
 
@@ -14,7 +12,8 @@ public class PrgUsuario extends GaneshProgram {
 
 	@Override
 	public ProgramDescriptor getProgramDescriptor() {
-		return new ProgramDescriptor("usuario", PrgUsuario.class, GM.usuario(), Menu.CADASTRO, Icons.USER);
+		return null;
+		//		return new ProgramDescriptor("usuario", PrgUsuario.class, GM.usuario(), Menu.CADASTRO, Icons.USER);
 	}
 
 }
