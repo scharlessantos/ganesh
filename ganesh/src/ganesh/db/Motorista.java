fc3118d6e3d2fcf79bc44dc5010aa500b154015a
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-06-01 11:52:37 +0000

    Motorista

--- ganesh/src/ganesh/db/Motorista.java
+++ ganesh/src/ganesh/db/Motorista.java
@@ -1,6 +1,40 @@
 /* Ganesh Server, developed in 2013*/
 package ganesh.db;
 
+import ganesh.common.exceptions.GException;
+import ganesh.common.request.RequestFilter.FilterType;
+import ganesh.db.annotations.Entity;
+import ganesh.db.annotations.Id;
+import ganesh.db.annotations.Property;
+import ganesh.db.utils.Filter;
+
+@Entity("motorista")
 public class Motorista extends Pessoa {
+
+	@Id("id_motorista")
+	private Long idMotorista;
+
+	@Property("id_empresa")
+	private Long idEmpresa;
+
+	public Long getIdEmpresa() {
+		return idEmpresa;
+	}
+
+	public long getIdMotorista() {
+		return idMotorista;
+	}
+
+	public Empresa getEmpresa() throws GException {
+		if (idEmpresa == null)
+			return null;
+
+		return DB.first(Empresa.class, new Filter(Empresa.class, "id_empresa", idEmpresa, FilterType.EQUALS));
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		if (empresa != null)
+			idEmpresa = empresa.getIdEmpresa();
+	}
 
 }
