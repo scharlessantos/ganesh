52a2a529d0cf3433912bd344c1609773f157de8a
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-06-19 00:22:04 +0000

    classe de banco para cliente

--- ganesh/src/ganesh/db/Cliente.java
+++ ganesh/src/ganesh/db/Cliente.java
@@ -1,10 +1,56 @@
 /* Ganesh Server, developed in 2013*/
 package ganesh.db;
 
+import ganesh.db.annotations.Id;
+import ganesh.db.annotations.Property;
+
 public class Cliente extends AbstractDBEntity {
+
+	@Id("id_cliente")
+	protected Long id;
+
+	@Property("codigo")
+	protected String codigo;
+
+	@Property("nome")
+	protected String nome;
+
+	@Property("endereco")
+	protected String endereco;
+
+	@Property("contato")
+	protected String contato;
+
+	@Property("responsavel")
+	protected Long responsavel;
 
 	@Override
 	protected void merge(AbstractDBEntity other) {
+		if (other instanceof Cliente) {
+
+			Cliente cliente = (Cliente)other;
+
+			if (this.id == null)
+				this.id = cliente.id;
+
+			if ((cliente.codigo != null && !cliente.codigo.equals(this.codigo)))
+				this.codigo = cliente.codigo;
+
+			if ((cliente.nome != null && !cliente.nome.equals(this.nome)))
+				this.nome = cliente.nome;
+
+			if ((cliente.codigo != null && !cliente.codigo.equals(this.codigo)))
+				this.codigo = cliente.codigo;
+
+			if ((cliente.responsavel != null && !cliente.responsavel.equals(this.responsavel)))
+				this.responsavel = cliente.responsavel;
+
+			if ((cliente.contato != null && !cliente.contato.equals(this.contato)))
+				this.contato = cliente.contato;
+
+			if ((cliente.endereco != null && !cliente.endereco.equals(this.endereco)))
+				this.endereco = cliente.endereco;
+		}
 
 	}
 
