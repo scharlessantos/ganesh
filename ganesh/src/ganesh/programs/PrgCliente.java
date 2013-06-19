ae001e3943043fa84dc19ac1cf732aa64ee0ab13
Charles Santos Silva <silva.charlessantos@gmail.com>
2013-06-19 00:40:01 +0000

    Prgs de Cliente e Pessoa

---
+++ ganesh/src/ganesh/programs/PrgCliente.java
@@ -0,0 +1,11 @@
+/* Ganesh Server, developed in 2013*/
+package ganesh.programs;
+
+public class PrgCliente extends GaneshProgram {
+
+	@Override
+	protected String getName() {
+		return "cliente";
+	}
+
+}
---
+++ ganesh/src/ganesh/programs/PrgPessoa.java
@@ -0,0 +1,11 @@
+/* Ganesh Server, developed in 2013*/
+package ganesh.programs;
+
+public class PrgPessoa extends GaneshProgram {
+
+	@Override
+	protected String getName() {
+		return "pessoa";
+	}
+
+}
